ac46a2a41ebd4b99a70ff97ae09c8bff8913b331
Xiang Wu <xiang.wu@hotmail.de>
2023-07-20 23:16:15 +0200

    structure

---
+++ components/layout/logo.js
@@ -0,0 +1,5 @@
+function Logo() {
+  return <div class="uppercase font-extrabold font-mono text-base">TCM</div>;
+}
+
+export default Logo;
---
+++ components/starting-page/starting-page.js
@@ -0,0 +1,15 @@
+function StartingPageContent() {
+  // Show Link to Login page if NOT auth
+
+  return (
+    <section>
+      <h1 class="absolute inset-0 flex justify-center items-center">
+        {/* input */}
+        <input />
+        {/* Search button */}
+      </h1>
+    </section>
+  );
+}
+
+export default StartingPageContent;
