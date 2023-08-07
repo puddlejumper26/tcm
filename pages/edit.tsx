52267580ae7951864b24cf4158b51a14dab6f6a4
Xiang <xiang.wu@joingsg.com>
2023-08-08 00:27:23 +0200

    test the git account

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,7 +1,7 @@
 import EditPage from "@/components/editing-page/EditPage";
 
 function Edit() {
-    // try git account
+    // try git accounts
   return (
     <div className="min-h-screen flex-col items-center p-24">
       <EditPage />
